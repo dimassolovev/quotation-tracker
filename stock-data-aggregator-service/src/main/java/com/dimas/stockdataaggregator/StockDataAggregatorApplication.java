1572b94b2f4b14d31a81b01fa5f1d892bc26f972
Dmitriy Solovev <ds3211407@gmail.com>
2024-09-17 18:56:52 +0300

    add stock-data-aggregator-service

---
+++ stock-data-aggregator-service/src/main/java/com/dimas/stockdataaggregator/StockDataAggregatorApplication.java
@@ -0,0 +1,11 @@
+package com.dimas.stockdataaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockDataAggregatorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StockDataAggregatorApplication.class, args);
+	}
+}
---
+++ stock-data-aggregator-service/src/test/java/com/dimas/stockdataaggregator/StockDataAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimas.stockdataaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockDataAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
