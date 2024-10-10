146692b4bf7675422f47d481a6bfb3dcfbdfd239
Dmitriy Solovev <ds3211407@gmail.com>
2024-10-10 19:01:51 +0300

    add moex-data-service

---
+++ moex-data-service/src/main/java/com/dimas/moexdataservice/MoexDataServiceApplication.java
@@ -0,0 +1,11 @@
+package com.dimas.moexdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoexDataServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MoexDataServiceApplication.class, args);
+    }
+}
